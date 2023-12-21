244c2d731a39d6c9d6c08c96d5cc387ffaf9a3ab
Yannis-1 <92175384+Yannis-1@users.noreply.github.com>
2023-12-21 13:52:26 +0100

    Add files via upload
    je kan nu niet bouwen als er al iets staat en de infinite money glitch is gepatcht

---
+++ 0.2/scripts/Home.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_pressed():
+	Global.autoload = true
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
